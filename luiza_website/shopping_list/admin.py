77a894a0d6b995cea07c9f74d53b71fac485b9c2
Ana Sanz <35330393+Ahisanz@users.noreply.github.com>
2020-11-17 00:53:26 +0000

    Adiciona base para o projeto final

---
+++ luiza_website/shopping_list/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+# from .models import Post
+from .models import Shopping_List
+
+# Register your models here.
+# admin.site.register(Post)
+admin.site.register(Shopping_List)
